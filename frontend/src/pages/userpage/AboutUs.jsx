6e75e94b51fd2c2ad75ca26e2f233d39b30fd596
karmapuzan <karmacharyapujan10@gmail.com>
2024-09-25 11:58:53 +0545

    about us

--- frontend/src/pages/userpage/AboutUs.jsx
+++ frontend/src/pages/userpage/AboutUs.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import UserContainer from '../../component/container/UserContainer'
+import "./About.css"
 
 const AboutUs = () => {
   return (
     <UserContainer>
-    <div>
+    <div className="about-us-container">
 
-        <h1>about us page</h1>
+    <section className="about-hero">
+        <div className="about-hero-content">
+        <h1>About Our Bike Rental</h1>
+        <div className="section-image">
+            <img src="img/rtr.png" alt="Our Mission" />
+          </div>
+        <p>
+            Ride in style with our premium motorbike rental services. Whether you're exploring the city or embarking on a scenic journey, we’ve got you covered with top-quality motorbikes.
+          </p>
+        </div>
       
+
+      <div className="contact-info">
+          <h2>Contact Us</h2>
+          <p>
+            Have any questions or need assistance with booking? <p>Feel free to reach out to us.</p>
+          </p>
+          <p>Email: info@bikerental.com</p>
+          <p>Phone: 9861586183</p>
+          <p>Address: Balakhu-2, Bhaktapur</p>
+        </div>
+      </section>
     </div>
     </UserContainer>
   )
